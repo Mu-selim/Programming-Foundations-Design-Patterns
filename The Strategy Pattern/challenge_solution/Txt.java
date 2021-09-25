be1612549f83b5b5c4ed327fe4d34d2d8a328c11
Muhammad Selim <89293751+Mu-selim@users.noreply.github.com>
2021-09-25 17:01:44 +0200

    Add files via upload

---
+++ The Strategy Pattern/challenge_solution/Txt.java
@@ -0,0 +1,9 @@
+package headfirst.designpatterns.strategy.challenge;
+
+public class Txt implements ShareStrategy 
+{
+	public void share() 
+	{
+		System.out.println("I'm txting the photo");
+	}
+}
